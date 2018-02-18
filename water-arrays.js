23cd9ae469b57d43e7295b76a79ba679578c7099
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-02-18 15:14:19 -0800

    Water Arrays

--- water-arrays.js
+++ water-arrays.js
@@ -1,34 +1,56 @@
-var x = 230;
-var y = 220;
+var a = 230;
+var b = 220;
+var r = 0;
+
+var x = [];
+var y = [];
 var ySpeed = [];
+
+x[0] = 10;
+y[0] = 10;
 ySpeed[0] = 3;
 
 function setup() {
   createCanvas(400, 400);
   colorMode(HSB)
-    for (var index = 0; index < 100; index = index + 1) {
-  x[index] = random(0,400);
-  y[index] = random(0,400);
+  for (var index = 0; index < 100; index = index + 1) {
+  x[index] = random(0,220);
+  y[index] = random(0,220);
   ySpeed[index] = random(3,5);
 }
 }
-  
+
 function draw() {
-  background(0);
+  background("gray");
   noStroke();
 
-  // draw pipe
-  rect(0, 200, x, 20);
+
+   // draw pipe
+  rect(0, 200, a, 20);
   
   // draw drip
-  ellipse(x, y, 10);
+  ellipse(a, b, 10);
   
-  // down 3 pixels each frame, but maybe should be accelerating?
-  y = y + 3
-  
-  // if invisible for a full “height” amount…
-  if (y > height*2) {
-    // reset
-    y = 220;
+      for (var index = 0; index < 100; index = index + 1) {
+      ellipse(x[index], y[index], 10, 10);
+      y[index] = y[index] + ySpeed[index];
+
+      if (y[index] > height) {
+        // reset
+        y[index] = 220;
+      }
+    }
+
+  // down 3 pixels
+  b = b + 3;
+
+  // rotate 0.05 radians ~= 2.8 degrees per frame
+  r += 0.04
+
+  // if reach past the bottom
+  if (b > height*2) {
+    b = 220;
+    
+   
   }
 }
